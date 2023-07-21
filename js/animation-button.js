5ea706656a08f0bf314ebad0e712bbbc80e54a19
Rashmeetk14 <rashmeet.kaur@simbaquartz.com>
2023-07-21 17:30:32 +0530

    first commit

--- js/animation-button.js
+++ js/animation-button.js
@@ -17,6 +17,7 @@ function clickHandler()
           let financialToggle_theme = document.getElementById('financialToggle_theme');
           let financalToggle = document.getElementById('financalToggle');
           let contacToggle_theme = document.getElementById('contacToggle_theme');
+          // let aboutTeamPageSection = documment.getElementById('about-teampage-section')
           
           // let insideNav=document.querySelector(".link-25")[0];
           // let insideNavIcon=document.querySelector(".icon-47")[0];
@@ -31,58 +32,119 @@ function clickHandler()
           financialToggle_theme.classList.toggle('navlink-2_1');
           financalToggle.classList.toggle('navlink-2_1');
           contacToggle_theme.classList.toggle('navlink-2_1');
+          // aboutTeamPageSection.classList.toggle('navlink-2_1');
           // logoBrand.src.toggle('images/SmileWhite.png')
-         let newImg =  document.querySelector('img[name="logoBrand"]');
-         newImg.src = 'images/SmileWhite.png';
+         let newImg =  document.querySelector('img[name="Whitemode-smileLogo"]');
+         newImg.src = 'images/Whitemode-smileLogo.png';
          newImg.sizes="(max-width: 991px) 100vw, (max-width: 1439px) 19vw, 260px";
-         newImg.srcset="images/SmileWhite.png 500w, images/SmileWhite.png 800w, images/SmileWhite.png 1080w, images/SmileWhite.png 1790w"
+         newImg.srcset="images/Whitemode-smileLogo.png 500w, images/Whitemode-smileLogo.png 800w, images/Whitemode-smileLogo.png 1080w, images/Whitemode-smileLogo.png 1790w"
           // insideNav.classList.toggle("link-25_1");
           // insideNavIcon.classList.toggle("icon-47_1");
 
-          //refresh button code
+         
         }
+
+      
         function clickHandler2(){
-          // setTimeout(() => {
-            let newImg =  document.querySelector('img[name="logoBrand"]');
-         newImg.src = 'images/smile.png';
-         newImg.sizes="(max-width: 991px) 100vw, (max-width: 1439px) 19vw, 260px";
-         newImg.srcset="images/smile-p-500.png 500w, images/smile-p-800.png 800w, images/smile-p-1080.png 1080w, images/smile.png 1790w"
+          setTimeout(() => {
             document.location.reload();
-          // }, 0);
+          }, 0);
         }
-        
+          //setTimeout(() => {
+            // let newImg =  document.querySelector('img[name="logoBrand"]');
+            // // newImg.src = 'images/smile.png';
+            // // newImg.sizes="(max-width: 991px) 100vw, (max-width: 1439px) 19vw, 260px";
+            // // newImg.srcset="images/smile-p-500.png 500w, images/smile-p-800.png 800w, images/smile-p-1080.png 1080w, images/smile.png 1790w"
+            // document.location.reload();
+          // }, 0);
+
+
+
         //increment
-        const incrementButton = document.getElementById("incrementButton");
-        const decrementButton = document.getElementById("decrementButton");
-        // Get all elements within the body
-        const allElements = document.body.getElementsByTagName("*");
-        // Get the current font sizes of all elements within the body
-        const getFontSizes = () => {
-          const fontSizes = [];
-          for (let i = 0; i < allElements.length; i++) {
-            const fontSize = parseFloat(
-              getComputedStyle(allElements[i]).fontSize
-            );
-            fontSizes.push(fontSize);
-          }
-          return fontSizes;
-        };
+        // const incrementButton = document.getElementById("incrementButton");
+        // const decrementButton = document.getElementById("decrementButton");
+        // // Get all elements within the body
+        // const allElements = document.body.getElementsByTagName("*");
+        // // Get the current font sizes of all elements within the body
+        // const getFontSizes = () => {
+        //   const fontSizes = [];
+        //   for (let i = 0; i < allElements.length; i++) {
+        //     const fontSize = parseFloat(
+        //       getComputedStyle(allElements[i]).fontSize
+        //     );
+        //     fontSizes.push(fontSize);
+        //   }
+        //   return fontSizes;
+        // };
      
-        // Set the font sizes for all elements within the body
-        const setFontSizes = (fontSizes) => {
-          for (let i = 0; i < allElements.length; i++) {
-            allElements[i].style.fontSize = fontSizes[i] + "px";
-          }
-        };
-        // Increase font sizes when the increment button is clicked
-        incrementButton.addEventListener("click", () => {
-          const currentFontSizes = getFontSizes();
-          const newFontSizes = currentFontSizes.map((fontSize) => fontSize + 1);
-          setFontSizes(newFontSizes);
-        });
-        // Decrease font sizes when the decrement button is clicked
-        decrementButton.addEventListener("click", () => {
-          const currentFontSizes = getFontSizes();
-          const newFontSizes = currentFontSizes.map((fontSize) => fontSize - 1);
-          setFontSizes(newFontSizes);
-        });
+        // // Set the font sizes for all elements within the body
+        // const setFontSizes = (fontSizes) => {
+        //   for (let i = 0; i < allElements.length; i++) {
+        //     allElements[i].style.fontSize = fontSizes[i] + "px";
+        //   }
+        // };
+        // // Increase font sizes when the increment button is clicked
+        // incrementButton.addEventListener("click", () => {
+        //   const currentFontSizes = getFontSizes();
+        //   const newFontSizes = currentFontSizes.map((fontSize) => fontSize + 1);
+        //   setFontSizes(newFontSizes);
+        // });
+        // // Decrease font sizes when the decrement button is clicked
+        // decrementButton.addEventListener("click", () => {
+        //   const currentFontSizes = getFontSizes();
+        //   const newFontSizes = currentFontSizes.map((fontSize) => fontSize - 1);
+        //   setFontSizes(newFontSizes);
+        // });
+
+    const incrementButton = document.getElementById("incrementButton");
+    const decrementButton = document.getElementById("decrementButton");
+   
+    const allElements = document.body.getElementsByTagName("*");
+    let clickCounter = 0;
+    const maxClicks = 6;
+
+    
+    const getFontSizes = () => {
+      const fontSizes = [];
+      for (let i = 0; i < allElements.length; i++) {
+        const fontSize = parseFloat(getComputedStyle(allElements[i]).fontSize);
+        fontSizes.push(fontSize);
+      }
+      return fontSizes;
+    };
+ 
+    const setFontSizes = (fontSizes) => {
+      for (let i = 0; i < allElements.length; i++) {
+        allElements[i].style.fontSize = fontSizes[i] + "px";
+      }
+    };
+
+    
+    incrementButton.addEventListener("click", (event) => {
+      event.preventDefault(); 
+      if (clickCounter < maxClicks) {
+        const currentFontSizes = getFontSizes();
+        const newFontSizes = currentFontSizes.map((fontSize) => fontSize + 1);
+        setFontSizes(newFontSizes);
+        clickCounter++;
+        if (clickCounter === maxClicks) {
+          incrementButton.disabled = true;
+        }
+        decrementButton.disabled = false; 
+      }
+    });
+
+  
+    decrementButton.addEventListener("click", (event) => {
+      event.preventDefault(); 
+      if (clickCounter > -maxClicks) {
+        const currentFontSizes = getFontSizes();
+        const newFontSizes = currentFontSizes.map((fontSize) => fontSize - 1);
+        setFontSizes(newFontSizes);
+        clickCounter--;
+        if (clickCounter === -maxClicks) {
+          decrementButton.disabled = true;
+        }
+        incrementButton.disabled = false; 
+      }
+    });
